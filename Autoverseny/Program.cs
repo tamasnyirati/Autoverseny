9684497026c21d606bb28407e9ad93dc46e23e01
TamasNyirati <tamas.nyirati@gmail.com>
2021-04-13 19:03:43 +0200

    Üres

---
+++ Autoverseny/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoverseny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
